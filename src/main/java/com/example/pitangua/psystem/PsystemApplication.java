8bf4d1641784523aca9c4f71217196e4568ccc60
Thais Lins <thaisfli@hotmail.com>
2018-06-02 02:11:51 -0300

    Initial commit

---
+++ src/main/java/com/example/pitangua/psystem/PsystemApplication.java
@@ -0,0 +1,12 @@
+package com.example.pitangua.psystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsystemApplication.class, args);
+	}
+}
